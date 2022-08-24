aaf63bf0aa27fc9d3611070eaf0679bd68f524d1
Sohail <kabeersohel@gmail.com>
2022-08-24 15:13:07 +0530

    1

--- app/src/main/java/com/example/screenshare/MainActivity.kt
+++ app/src/main/java/com/example/screenshare/MainActivity.kt
@@ -1,28 +1,22 @@
 package com.example.screenshare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import android.view.KeyEvent
-import android.widget.Toast
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.screenshare.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main   )
-    }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        return if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP){
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-            true
-        } else {
-            super.onKeyDown(keyCode, event)
-        }
+        val myCanvasView = MyCanvas(this)
+        myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
-
 }
