91e6cef6ed3df62666e57edb272c3f5840ddf725
Sohail <kabeersohel@gmail.com>
2022-08-24 15:09:43 +0530

    transparent activity

--- app/src/main/java/com/example/screenshare/MainActivity.kt
+++ app/src/main/java/com/example/screenshare/MainActivity.kt
@@ -1,19 +1,19 @@
 package com.example.screenshare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.screenshare.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main   )
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
