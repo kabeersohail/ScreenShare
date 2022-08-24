cd487ae7304f7a4f548f55bbb52bcbcbfdf896c2
Sohail <kabeersohel@gmail.com>
2022-08-24 14:11:11 +0530

    drawing on the canvas is done with a rectangle

--- app/src/main/java/com/example/screenshare/fragments/LaunchFragment.kt
+++ app/src/main/java/com/example/screenshare/fragments/LaunchFragment.kt
@@ -3,8 +3,12 @@ package com.example.screenshare.fragments
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.screenshare.MyCanvas
+import com.example.screenshare.R
 import com.example.screenshare.databinding.FragmentLaunchBinding
 
 class LaunchFragment : Fragment() {
@@ -17,6 +21,20 @@ class LaunchFragment : Fragment() {
     ): View {
         binding = FragmentLaunchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    @Suppress("Deprecation")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // To hide action bar
+        val activity: AppCompatActivity? = activity as AppCompatActivity?
+        activity?.supportActionBar?.hide()
+
+        val myCanvasView = MyCanvas(requireContext())
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        requireActivity().setContentView(myCanvasView)
     }
 
 }
