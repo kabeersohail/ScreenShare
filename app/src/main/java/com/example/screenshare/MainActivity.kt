6e3ecd853321e1fca629a047b112a188b0e409cb
Sohail <kabeersohel@gmail.com>
2022-08-25 11:42:11 +0530

    in progress

--- app/src/main/java/com/example/screenshare/MainActivity.kt
+++ app/src/main/java/com/example/screenshare/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.screenshare
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import com.example.screenshare.databinding.ActivityMainBinding
 
@@ -10,6 +15,7 @@ class MainActivity : Activity() {
 
     lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -18,5 +24,10 @@ class MainActivity : Activity() {
         myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
         setContentView(myCanvasView)
+
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+            startActivity(intent)
+        }
     }
 }
