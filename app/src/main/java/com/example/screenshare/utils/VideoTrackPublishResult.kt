af273455cbf5f324e12ffdb033154d5422f000ba
Sohail <kabeersohel@gmail.com>
2022-09-22 21:52:27 +0530

    start branch

--- app/src/main/java/com/example/screenshare/utils/VideoTrackPublishResult.kt
+++
@@ -1,8 +0,0 @@
-package com.example.screenshare.utils
-
-import com.twilio.video.TwilioException
-
-sealed class VideoTrackPublishResult {
-    object Success : VideoTrackPublishResult()
-    class Failure(val twilioException: TwilioException) : VideoTrackPublishResult()
-}
